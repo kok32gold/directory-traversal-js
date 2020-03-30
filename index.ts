459d28b986056f597f6e4a1429cbca4f41663a00
Ben Ari Kutai <benari.k@claroty.com>
2020-03-30 11:02:49 +0300

    revert changes

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import * as path from 'path';
 
+function startsWith(txt: string, search: string, rawPos?: number) {
+    const pos: number = rawPos > 0 ? rawPos|0 : 0;
+    return txt.substring(pos, pos + search.length) === search;
+}
+
 function commonPrefix(paths: string[]): string {
     const arr: string[] = paths.concat().sort();
     const startElement: string = arr[0];
@@ -15,7 +20,7 @@ export function safeJoin(rootFolder: string, filename: string): string {
     rootFolder = path.join(path.normalize(rootFolder), '');
     const normalizedFullPath: string = path.normalize(path.join(rootFolder, filename));
     const commonPathPrefix: string = commonPrefix([normalizedFullPath, rootFolder]);
-    if (!commonPathPrefix || !commonPathPrefix.startsWith(rootFolder)) {
+    if (!commonPathPrefix || !startsWith(commonPathPrefix, rootFolder)) {
         throw new Error("Requested filename is outside of root directory!");
     }
     return normalizedFullPath;
